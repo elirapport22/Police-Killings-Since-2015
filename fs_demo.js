b5b8ba5ff55b572fab6c3ccecf13b5a55098996b
musaJallow22 <56697206+musaJallow22@users.noreply.github.com>
2021-11-19 11:21:59 -0500

    Got rid of "/r" issue.

--- fs_demo.js
+++ fs_demo.js
@@ -3,29 +3,29 @@ const fs = require('fs');
 let rawData = [];
 
 let rawData_csv = fs.readFileSync('fatal-police-shootings-data.csv', 'utf8');
-
-let policeData = rawData_csv.split("\n");
+let policeData = rawData_csv.split(/[\n\r]+/);
 
 policeData.forEach(function(data) {
   let raw_info = data.split(',');
   let personData = {};
-  personData['id'] = raw_info[0];
-  personData['name'] = raw_info[1];
-  personData['date'] = raw_info[2];
-  personData['armed'] = raw_info[3];
-  personData['age'] = raw_info[4];
-  personData['gender'] = raw_info[5];
-  personData['race'] = raw_info[6];
-  personData['state'] = raw_info[7];
-  personData['mental_illness'] = raw_info[8];
-  personData['threat_level'] = raw_info[9];
-  personData['flee'] = raw_info[10];
-  personData['longitude'] = raw_info[11];
-  personData['latitude'] = raw_info[12];
+  personData["id"] = raw_info[0];
+  personData["name"] = raw_info[1];
+  personData["date"] = raw_info[2];
+  personData["armed"] = raw_info[3];
+  personData["age"] = raw_info[4];
+  personData["gender"] = raw_info[5];
+  personData["race"] = raw_info[6];
+  personData["state"] = raw_info[7];
+  personData["mental_illness"] = raw_info[8];
+  personData["threat_level"] = raw_info[9];
+  personData["flee"] = raw_info[10];
+  personData["longitude"] = raw_info[11];
+  personData["latitude"] = raw_info[12];
 
   rawData.push(personData);
-  rawData.trim();
+
 
 });
 
-fs.writeFileSync('crime.json', JSON.stringify(rawData), 'utf8');
+let dataString = JSON.stringify(rawData);
+fs.writeFileSync('crime.json', dataString, 'utf8');
