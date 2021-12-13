7881702694084a7557ed3ee1f4ef31d56607b0dd
musaJallow22 <56697206+musaJallow22@users.noreply.github.com>
2021-12-13 03:09:28 -0500

    Revamped site.

--- data/generateJSON.js
+++ data/generateJSON.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let finalData = [];
 let rawData_csv = fs.readFileSync('fatal-police-shootings-data.csv', 'utf8');
 let policeData = rawData_csv.split("\n");
-let arrayOfStateNames = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"];
+let arrayOfStateNames = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","ME","MD","MA","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WV","WI","WY"];
 let arrayOfEachStatesData = [];
 let ak = [];
 
